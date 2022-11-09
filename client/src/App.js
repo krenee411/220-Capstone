46e3ed21b72c193d7edb8d2455eafce9af0039a1
michael-pulliam <michael.pulliam@bryanuniversity.edu>
2022-11-08 22:58:34 -0600

    setup react-router-dom & Routes to Landing, Blog, & About Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
+import {createContext} from 'react'
+import { BrowserRouter, Route, Routes, Link} from 'react-router-dom'
+import Landing from './Landing/Landing.js';
+import About from './About/About';
+import Blog from './Blog/Blog';
 
+const UserContext = createContext()
 function App() {
+
   return (
     <div>
-      
+
+       <BrowserRouter>
+        <UserContext.Provider value={{}}>
+          <Link to="/">Home</Link>
+
+          <Link to="/blog">Blogs</Link>
+
+          <Link to="/about">About</Link>
+
+          <Routes>
+
+            <Route element={<Landing/>} exact path='/'></Route>
+
+            <Route element={<About/>} exact path='/about'></Route>
+
+            <Route element={<Blog/>} exact path='/blog'></Route>
+
+          </Routes>
+
+        </UserContext.Provider>
+
+
+
+       </BrowserRouter>
+
     </div>
   );
 }
---
+++ client/src/Landing/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div><h1>Home</h1>
+        <p>Welcome to our First Family Project Management HOME Page!!</p>
+</div>
+  )
+}
+
+export default Landing
