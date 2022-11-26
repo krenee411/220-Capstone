8b3255f35e56e16afa30eb38ec1d68bb728e37f6
michael-pulliam <michael.pulliam@bryanuniversity.edu>
2022-11-25 20:45:07 -0600

    update

--- client/src/MemoriesForm/MemoriesForm.js
+++ client/src/MemoriesForm/MemoriesForm.js
@@ -1,31 +1,50 @@
-import React from "react"
+import React, {useState} from "react"
+import axios from "axios";
 
 export default function MemoriesForm(){
+    const [memory, setMemory] = useState({
+        title: '',
+        message: '',
+    });
+
+    function handleChange(event){
+        const { name, value } = event.target;
+        setMemory((prevInput) => {
+            return {
+                ...prevInput,
+                [name]: value,
+            }
+        });
+        console.log(memory);
+    }
+
+    function addMemory(event){
+        event.preventDefault()
+        const newMemory = {
+            title: memory.title,
+            message: memory.title,
+        };
+        axios.post('/newmemory', newMemory);
+        
+    }
     return(
         <form>
             <div>
-            <input
-                type='text'
-                name='creator'
-                // value={Creator}
-                // onChange={handleChange}
-                placeholder='Creator'
-                />
                 <input
                 type='text'
                 name='title'
-                // value={title}
-                // onChange={handleChange}
+                value={memory.title}
+                onChange={handleChange}
                 placeholder='Title'
                 />
                 <input
                 type='text'
                 name='message'
-                // value={message}
-                // onChange={handleChange}
+                value={memory.message}
+                onChange={handleChange}
                 placeholder='message'
                 />
-                <button>Post Memory</button>
+                <button onClick={addMemory}>Post Memory</button>
                 
             </div>
         </form>
