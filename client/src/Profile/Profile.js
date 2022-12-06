1fc27735ab79574082b7590fa896c9dbae1f95e6
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2022-12-05 21:04:06 -0600

    Uncommented token code for Profile.js for Churchill

--- client/src/Profile/Profile.js
+++ client/src/Profile/Profile.js
@@ -1,18 +1,18 @@
 import React, { useState, useContext } from 'react'; 
-// import {UserContext} from '../Contexts/UserContext' 
+import {UserContext} from '../Context/UserProvider';
 function Profile() { 
     const [profilePic, setProfilePic] = useState(''); 
     // const [username, setUsername] = useState(''); 
     // const [email, setEmail] = useState(''); // 
     const [password, setPassword] = useState(''); 
-    // const { token } = useContext(UserContext) 
-    // console.log(token) 
+    const { token } = useContext(UserContext) 
+    console.log(token) 
     return ( 
     <> 
     <h1>Your Profile</h1>
     <img src="https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=170667a&w=0&k=20&c=MRMqc79PuLmQfxJ99fTfGqHL07EDHqHLWg0Tb4rPXQc=" alt="Girl in a jacket" width="250" height="150"/> 
     <div> 
-        {/* <h2>Username: {token}</h2> */}
+        <h2>Username: {token}</h2>
         <h2>Email:</h2> 
         <h2>Password:</h2>
         </div> 
