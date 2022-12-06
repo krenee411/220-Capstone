1996c4b406966640d6ff82f791ee87982cf2e453
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2022-12-05 20:48:31 -0600

    Returned Churchill's code

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,38 @@
 
 import './App.css';
-import React, { useContext } from 'react';
-import { Route, Routes, Navigate} from 'react-router-dom'
+import React, { useState } from 'react';
+import { Route, Routes} from 'react-router-dom'
 import Landing from './Landing/Landing.js';
 import About from './About/About.js';
+import Memories from './Memories/Memories.js';
 import NavBar from './NavBar/NavBar.js'
 import RenderForm from './Auth/RenderForm'
-import MemoriesDisplay from './MemoriesDisplay/MemoriesDisplay.js';
-import { UserContext } from "./Context/UserProvider.js"
+import Profile from './Profile/Profile'
 
-// export const UserContext = createContext()
-
-
+const getUserToken = () => {
+  const string = sessionStorage.getItem('token')
+  const token = JSON.parse(string);
+  return token?.token
+}
 
 function App() {
-  const { user, token} = useContext(UserContext)
-console.log(user, token)
+  const [token, setToken] = useState(getUserToken)
 
-  // if(!token){
-  //   return <RenderForm setToken={setToken}/>
-  // }
+  const setUserToken = userToken => {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+    setToken(userToken)
+  }
   
   return (
     <div>
-      {/* <UserContext.Provider value={{user, setUser}}> */}
        <NavBar />
        <Routes>
          <Route element={<Landing/>} exact path='/'></Route>
          <Route element={<About/>} path='/about'></Route>
-         <Route element={<MemoriesDisplay/>} path='/memoriesDisplay'></Route>
-         <Route element={token ? <Navigate to='/memoriesDisplay'/> : <RenderForm/>} path = '/login'></Route>
-       </Routes>
-       {/* </UserContext.Provider>   */}
+         <Route element={<Memories/>} path='/Memories'></Route>
+         <Route element={<RenderForm setToken={setUserToken}/>} path = '/login'></Route>
+         <Route element={<Profile/>} path='/profile'></Route>
+       </Routes>  
     </div>
   );
 }
