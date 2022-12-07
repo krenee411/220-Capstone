8e20ab28d83b4435b0fa938e80406d42836680ea
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-12-07 16:57:10 -0600

    Kelcis Commit

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(morgan('dev'));
 
 //Routes
 app.use('/auth', require('./routes/authRouter.js'));
+app.use('/api/routes', require('./routes/blogRouter.js'))
 app.use('/api', expressJwt({ secret: process.env.SECRET, algorithms: ['HS256'] }));
 app.use('/api/gallery', require('./routes/galleryRouter.js'));
 app.use('/public/memories', require('./routes/memoriesRouter.js'));
